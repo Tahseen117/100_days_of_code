dabe8dc7ed893d3e111a5047a04fc37fd084578a
Tahseen117 <tahskhan117@gmail.com>
2022-08-23 02:53:19 +0530

    modified turtle_race file

--- turtle_race/main.py
+++ turtle_race/main.py
@@ -4,7 +4,6 @@ from turtle import Turtle, Screen
 screen = Screen()
 screen.setup(width=800,height=500)
 user_bet= screen.textinput(title="Make your bet", prompt="Who will win the race. Enter your colour :")
-print(user_bet)
 
 color = ['red', 'blue', 'green', 'orange', 'purple']
 y_coordinate = [200, 100, 0, -100, -200]
@@ -14,7 +13,7 @@ for turtle in range(0,5):
     new_turtle = Turtle(shape="turtle")
     new_turtle.penup()
     new_turtle.color(color[turtle])
-    #new_turtle.speed("fastest")
+    new_turtle.speed("fastest")
     new_turtle.goto(-390,y_coordinate[turtle])
     turtles.append(new_turtle)
 
